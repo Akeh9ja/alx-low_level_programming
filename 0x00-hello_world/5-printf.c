e41c449746d24c9b70929990a2c3083dd16c6238
Akeh9ja <wilfreddbest1@gmail.com>
2022-03-10 15:24:54 -0800

    Task 6 pushed

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
-/*
- *main function entry 
+/**
+ *main - Entry point 
  *
- * return: 0
- * */
+ * return: Always 0
+ * /
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
