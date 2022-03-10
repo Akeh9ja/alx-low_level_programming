d1a3f2ad7379030d6654620eb611df8050060fdb
Akeh9ja <wilfreddbest1@gmail.com>
2022-03-10 14:44:28 -0800

    adding task 5

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+/*header function above
+ *
+ *
+ *
+ * Main integer function below
+ */
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");/*puts function returns new line by default*/
+	return (0); /*returning zero*/
+
+}
