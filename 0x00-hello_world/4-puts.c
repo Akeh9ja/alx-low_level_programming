dc659251d6be00e0783c15126e0f7d27b0712aff
Akeh9ja <wilfreddbest1@gmail.com>
2022-03-10 14:58:12 -0800

    still worked on the fifth task

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include<stdio.h>
 /**
- *Main - Entry Point
+ *Main - Entry point
  *
  *
  * Return: Always 0 (Sucess)
  */
-int main (void)
+int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
