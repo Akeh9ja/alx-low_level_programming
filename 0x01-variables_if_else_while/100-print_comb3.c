45b9678a9a0c655539984819a2dec775433ef324
Akeh9ja <wilfreddbest1@gmail.com>
2022-03-11 20:45:25 -0800

    Advance task 1

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main-prints all possible combination of two numbers
+ * and in ascending order
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = 48; n <= 56; n++)
+
+	{
+		for (n = 49; m <= 57; m++)
+		{
+			if (m > n)
+			{
+				putchar(n);
+				putchar(m);
+				if (n != 56 || m != 57)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
