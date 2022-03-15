c9afc6bb9e8795b84057b8346896ad09c57d0fec
Akeh9ja <wilfreddbest1@gmail.com>
2022-03-15 10:55:51 -0700

    Task three added

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet_x10 - print all lower case letters ten times,
+ * follow by new line
+ */
+void print_alphabet_x10(void)
+{
+	char ch;
+	int  i;
+
+	i = 0;
+
+	while
+		(i < 10);
+	{
+		ch = 'a';
+
+		while
+			(ch <= 'z');
+		{
+			putchar(ch);
+			ch++;
+		}
+		putchar('\n');
+		i++;
+	}
+}
