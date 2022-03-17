c0af0cc19629bd89f82407e1065806321504547b
Akeh9ja <wilfreddbest1@gmail.com>
2022-03-17 03:20:29 -0700

    Debugging task 0 added

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -10,20 +10,7 @@ int main(void)
 	int i;
 
 	i = 0;
-
-
-	if (i > 0)
-	{
-		printf("%d is positive\n", i);
-	}
-	else if (i < 0)
-	{
-		printf("%d\n", i);
-	}
-	else
-	{
-		printf("%d is zero\n", i);
-	}
+	positive_or_negative(i);
 
 	return (0);
 }
